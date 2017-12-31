b175e42c0e382666595178e5c15d2b9aeafdb303
Fi2zz <fi2zz@outlook.com>
2017-12-31 16:23:44 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,41 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
-  },
-  module: {
-    rules: [
-      // {
-      //   test: /\.css$/,
-      //   use: [
-      //     'stylus-loader',
-      //     'css-loader'
-      //   ],
-      // },
-      // {
-      //   test: /\.(styl|stylus)$/,
-      //   use: [
-      //     {loader: "stylus-loader"},
-      //     // {loader: 'css-loader'},
-      //     {loader: "style-loader"},
-      //   ],
-      // },
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(stylus|styl)$/,
+                loader: 'style-loader!css-loader!stylus-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.json']
+    },
 
-      {test: /\.(stylus|styl)$/, loader: 'style-loader!css-loader!stylus-loader'},
-
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.json']
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map'
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true,
+        overlay: true
+    },
+    devtool: '#eval-source-map'
 };
 
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
-}
